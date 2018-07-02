c86bd587d1b90cd0c1070d682a5dd813f3cd5df7
gavin_active <gavin.ligthart@activecollective.nl>
2018-07-02 14:27:54 +0200

    Added webpack and babel configuration. Loading categories in home view

---
+++ app/components/CategoryLabel.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class CategoryLabel extends React.Component {
+  static propTypes = {
+    category: PropTypes.string.isRequired,
+  };
+
+  getCategoryName() {
+    const { category } = this.props;
+    return category[0].toUpperCase() + category.slice(1);
+  }
+
+  render() {
+    const { category } = this.props;
+
+    return (
+      <span>
+        <a href={category}>
+          {this.getCategoryName()}
+        </a>
+      </span>
+    );
+  }
+}
+
+export default CategoryLabel;
--- app/pages/Home.js
+++ app/pages/Home.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import axios from 'axios';
+
+import CategoryLabel from '../components/CategoryLabel';
+
 import logo from '../assets/images/star-wars-logo.png';
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       title: 'Starwars Explorer',
+      categories: {},
     };
   }
 
+  componentDidMount() {
+    axios.get('https://swapi.co/api/')
+      .then((response) => {
+        this.setState({ categories: response.data });
+      })
+      .catch(() => {
+        // Handle error
+      });
+  }
+
   render() {
-    const { title } = this.state;
+    const { title, categories } = this.state;
 
     return (
       <div>
         <h2 id="heading">{ title }</h2>
+
         <img
           className="image"
           style={{ margin: '0.5em' }}
@@ -23,6 +39,13 @@ class Home extends React.Component {
           src={logo}
           alt="Star Wars Logo"
         />
+
+        {Object.keys(categories).map(cat => (
+          <CategoryLabel
+            key={`cat-list-${cat}`}
+            category={cat}
+          />
+        ))}
       </div>
     );
   }
