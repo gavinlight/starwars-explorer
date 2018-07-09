829015a35fd5155ab3b39d5e2b329915dff5554b
gavin_active <gavin.ligthart@activecollective.nl>
2018-07-10 01:42:15 +0200

    Refactored card component to a stateless function

--- app/components/Card/Card.js
+++ app/components/Card/Card.js
@@ -4,62 +4,58 @@ import PropTypes from 'prop-types';
 import MetaData from './Details/MetaData';
 import RelatedData from './Details/RelatedData';
 
-class Card extends React.Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    tag: PropTypes.string,
+const Card = ({
+  title, tag, url, metaData, relatedData,
+}) => {
+  return (
+    <article>
+      <h1><a href={url}>{title}</a></h1>
+      <h2>{tag}</h2>
+
+      { relatedData.length > 0 && (
+        <ul>
+          { metaData.map(meta => (
+            <li key={`${meta.name}-${meta.value}`}>
+              <MetaData name={meta.name} value={meta.value} />
+            </li>
+          )) }
+        </ul>
+      ) }
+
+      { relatedData.length > 0 && (
+        <ul>
+          { relatedData.map(related => (
+            <li key={`${related.type}-${related.url}`}>
+              <RelatedData type={related.type} url={related.url} />
+            </li>
+          )) }
+        </ul>
+      ) }
+
+    </article>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  tag: PropTypes.string,
+  url: PropTypes.string.isRequired,
+
+  metaData: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+  })),
+
+  relatedData: PropTypes.arrayOf(PropTypes.shape({
+    type: PropTypes.string.isRequired,
     url: PropTypes.string.isRequired,
+  })),
+};
 
-    metaData: PropTypes.arrayOf(PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      value: PropTypes.string.isRequired,
-    })),
-
-    relatedData: PropTypes.arrayOf(PropTypes.shape({
-      type: PropTypes.string.isRequired,
-      url: PropTypes.string.isRequired,
-    })),
-  };
-
-  static defaultProps = {
-    tag: '',
-    metaData: [],
-    relatedData: [],
-  }
-
-  render() {
-    const {
-      title, tag, url, metaData, relatedData,
-    } = this.props;
-
-    return (
-      <article>
-        <h1><a href={url}>{title}</a></h1>
-        <h2>{tag}</h2>
-
-        { relatedData.length > 0 && (
-          <ul>
-            { metaData.map(meta => (
-              <li key={`${meta.name}-${meta.value}`}>
-                <MetaData name={meta.name} value={meta.value} />
-              </li>
-            )) }
-          </ul>
-        ) }
-
-        { relatedData.length > 0 && (
-          <ul>
-            { relatedData.map(related => (
-              <li key={`${related.type}-${related.url}`}>
-                <RelatedData type={related.type} url={related.url} />
-              </li>
-            )) }
-          </ul>
-        ) }
-
-      </article>
-    );
-  }
-}
+Card.defaultProps = {
+  tag: '',
+  metaData: [],
+  relatedData: [],
+};
 
 export default Card;
