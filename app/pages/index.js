825b784de8d4fc70cf0e621245372c294d4e4855
gavin_active <gavin.ligthart@activecollective.nl>
2018-07-11 13:26:02 +0200

    Removed logo from header and codebase

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import logo from '../assets/images/star-wars-logo.png';
-
 const Page = ({ title, children }) => {
   return (
     <div>
       <header>
-        <img src={logo} alt="Star Wars Logo" />
         <h1>{ title }</h1>
       </header>
       { children }
